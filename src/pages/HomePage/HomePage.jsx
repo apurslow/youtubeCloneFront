b4de68bcf51ce46fe7f47ac021c02a26efeadbbd
kingbsolomon <bjsolomon@gmail.com>
2022-06-16 16:27:50 -0400

    merge conflicts

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -12,11 +12,7 @@ const HomePage = () => {
   const { user } = useContext(AuthContext);
   const [search, setSearch] = useState(null);
   const [videoId, setVideoId] = useState(null);
-<<<<<<< HEAD
   const api = 'AIzaSyDLSV2GMYdeeeoaTK7-qF443k3VXMbBPUY';
-=======
-  const api = 'AIzaSyDn97ZAkDkv5ptUzvUSW_sFht-C1IlR588';
->>>>>>> 3f9562071c9785b3ef02d46e92c8005bc36cd8d6
 
   const videoSearch = () => {
     if(search)
@@ -32,7 +28,6 @@ const HomePage = () => {
     <div id="=mainContent" align="center">
       <h1 className="container">Home Page for {user.name}!</h1>
       <SearchBar setSearch={setSearch}/>
-<<<<<<< HEAD
       
         <VideoDisplay 
         width="650"
@@ -41,7 +36,6 @@ const HomePage = () => {
         />
       
       <NewPost videoId={videoId}/>
-=======
       {videoId &&
       <>
       <VideoDisplay 
@@ -50,7 +44,6 @@ const HomePage = () => {
         videoId={videoId}
       />
       <TitleDescription videoId={videoId} api={api} />
->>>>>>> 3f9562071c9785b3ef02d46e92c8005bc36cd8d6
       <Post videoId={videoId}/>
       <RelatedVideos videoId={videoId} api={api} />
       </>
